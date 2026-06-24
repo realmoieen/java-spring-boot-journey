c676546c9ff950d575d1f2cb7b249e9d66491dd6
Zia1010 <mziam155@gmail.com>
2026-06-24 14:12:28 +0500

    Calculator practice

---
+++ src/main/java/com/github/zia/DivisionByZeroException.java
@@ -0,0 +1,7 @@
+package com.github.zia;
+
+public class DivisionByZeroException extends RuntimeException {
+public DivisionByZeroException (String message){
+    super(message);
+}
+}
---
+++ src/main/java/com/github/zia/InvalidInputException.java
@@ -0,0 +1,6 @@
+package com.github.zia;
+    public class InvalidInputException extends RuntimeException {
+        public InvalidInputException (String message){
+            super(message);
+        }
+    }
